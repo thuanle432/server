7cc8d9feb9661a3b3eab9624667137eeab00df6b
Lê Kim Thuận <thuanle@asia.icsc.vn>
2024-05-13 14:57:34 +0700

    update code server

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const userRoutes = require('./routes/UserRouter');
 const TourRouter = require('./routes/TourRouter')
 const app = express();
 const port = 3001;
+app.get("/", (req, res)=>{
+        res.send("Hello server")
+})
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 app.use(bodyParser.json());
