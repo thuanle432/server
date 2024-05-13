02ff94126836ffc9b10a2c9805512da4b3984c99
Lê Kim Thuận <thuanle@asia.icsc.vn>
2024-05-13 15:51:49 +0700

    update continue

--- config/Database.js
+++ config/Database.js
@@ -1,11 +1,13 @@
 
 const mysql = require('mysql');
+require('dotenv').config();
+
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'haitien'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 module.exports = pool;
